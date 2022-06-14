3cadd804ae7f4b072bbca14f816446236d0be4ef
YAZAN <YAZAN@DESKTOP-SVUSALT>
2022-06-15 00:42:28 +0300

    Add project files.

---
+++ LaTow/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace LaTow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtusernae = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.txtconpass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataView1 = new System.Windows.Forms.DataGridView();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btnUpdata = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(205, 47);
+            this.txtID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(154, 22);
+            this.txtID.TabIndex = 0;
+            // 
+            // txtusernae
+            // 
+            this.txtusernae.Location = new System.Drawing.Point(205, 91);
+            this.txtusernae.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtusernae.Name = "txtusernae";
+            this.txtusernae.Size = new System.Drawing.Size(154, 22);
+            this.txtusernae.TabIndex = 0;
+            this.txtusernae.TextChanged += new System.EventHandler(this.txtusernae_TextChanged);
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(205, 132);
+            this.txtpassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(154, 22);
+            this.txtpassword.TabIndex = 0;
+            // 
+            // txtconpass
+            // 
+            this.txtconpass.Location = new System.Drawing.Point(205, 170);
+            this.txtconpass.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtconpass.Name = "txtconpass";
+            this.txtconpass.Size = new System.Drawing.Size(154, 22);
+            this.txtconpass.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(128, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 97);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "user name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(109, 135);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(109, 176);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "password";
+            // 
+            // dataView1
+            // 
+            this.dataView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataView1.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dataView1.Location = new System.Drawing.Point(404, 47);
+            this.dataView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataView1.Name = "dataView1";
+            this.dataView1.RowHeadersWidth = 51;
+            this.dataView1.Size = new System.Drawing.Size(191, 141);
+            this.dataView1.TabIndex = 2;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(103, 251);
+            this.btn_add.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(100, 28);
+            this.btn_add.TabIndex = 3;
+            this.btn_add.Text = "add";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(271, 251);
+            this.btn_delete.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(100, 28);
+            this.btn_delete.TabIndex = 3;
+            this.btn_delete.Text = "delete";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // btnUpdata
+            // 
+            this.btnUpdata.Location = new System.Drawing.Point(462, 251);
+            this.btnUpdata.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnUpdata.Name = "btnUpdata";
+            this.btnUpdata.Size = new System.Drawing.Size(100, 28);
+            this.btnUpdata.TabIndex = 3;
+            this.btnUpdata.Text = "Updata";
+            this.btnUpdata.UseVisualStyleBackColor = true;
+            this.btnUpdata.Click += new System.EventHandler(this.btnUpdata_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 335);
+            this.Controls.Add(this.btnUpdata);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.dataView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtconpass);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtusernae);
+            this.Controls.Add(this.txtID);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "User";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtusernae;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.TextBox txtconpass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataView1;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btnUpdata;
+    }
+}
+
---
+++ LaTow/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaTow
+{
+    public partial class Form1 : Form
+    {
+        static SqlConnection Connection = new SqlConnection("data source = DESKTOP-DELL\\SQLEXPRESS; initial catalog=IM; persist security info=True; Integrated Security = SSPI;");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            Connection.Open();
+            SqlCommand comd = new SqlCommand("insert into [user] values @user,@password,@conpassword", Connection);
+ 
+            comd.Parameters.AddWithValue("@user", txtusernae.Text);
+            comd.Parameters.AddWithValue("@password", txtpassword.Text);
+            comd.Parameters.AddWithValue("@conpassword", txtconpass.Text);
+            comd.ExecuteNonQuery();
+            Connection.Close();
+            MessageBox.Show("add the rec has been sussfully");
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            Connection.Open();
+            SqlCommand comd = new SqlCommand("Delete [user] where ID=@ID", Connection);
+            comd.Parameters.AddWithValue("@ID", int.Parse(txtID.Text));
+            comd.ExecuteNonQuery();
+            Connection.Close();
+            MessageBox.Show("Delete the rec has been sussfully");
+        }
+
+        private void btnUpdata_Click(object sender, EventArgs e)
+        {
+            Connection.Open();
+            SqlCommand comd = new SqlCommand("update [user] set User=@user,password=@password where ID=@ID", Connection);
+            comd.Parameters.AddWithValue("@ID", int.Parse(txtID.Text));
+            comd.Parameters.AddWithValue("@User", txtusernae.Text);
+            comd.Parameters.AddWithValue("@password", txtpassword.Text);
+            comd.Parameters.AddWithValue("@conpassword", txtconpass.Text);
+            comd.ExecuteNonQuery();
+            Connection.Close();
+            MessageBox.Show("update the rec has been sussfully");
+        }
+
+        private void txtusernae_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
